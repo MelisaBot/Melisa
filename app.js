73d6ae49ed907e7db791fc62518399443a0fddf3
Max <maxicespedes7@hotmail.com>
2016-06-25 23:30:16 -0300

    Adedd

--- app.js
+++ app.js
@@ -15,6 +15,10 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// Aca requiero los requiero las deps del Bot
+var restify = require('restify');
+var builder = require('botbuilder');
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -56,6 +60,20 @@ app.use(function(err, req, res, next) {
     message: err.message,
     error: {}
   });
+});
+
+
+// Mi Bot :) Melisa
+var bot = new builder.BotConnectorBot({ appId: 'YourAppId', appSecret: 'YourAppSecret' });
+bot.add('/', function (session) {
+  session.send('Hola mundo');
+});
+
+// Setup Restify Server
+var server = restify.createServer();
+server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
+server.listen(process.env.port || 3978, function () {
+  console.log('%s listening to %s', server.name, server.url);
 });
 
 
---
+++ public/javascripts/sugerencias.js
@@ -0,0 +1,6 @@
+var myApp = angular.module('mlapp', []);
+
+myApp.controller('mlcontroller', ['$scope', function($scope) {
+    $scope.cities=["sdfsdfd", "sdf"];
+    }
+]);
