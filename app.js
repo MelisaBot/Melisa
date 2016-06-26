36b90585305b9dbdb64c536df340d6fbbf78601b
Max <maxicespedes7@hotmail.com>
2016-06-26 00:44:26 -0300

    Luis integrado

--- app.js
+++ app.js
@@ -15,10 +15,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// Aca requiero los requiero las deps del Bot
-var restify = require('restify');
-var builder = require('botbuilder');
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -60,20 +56,6 @@ app.use(function(err, req, res, next) {
     message: err.message,
     error: {}
   });
-});
-
-
-// Mi Bot :) Melisa
-var bot = new builder.BotConnectorBot({ appId: 'YourAppId', appSecret: 'YourAppSecret' });
-bot.add('/', function (session) {
-  session.send('Hola mundo');
-});
-
-// Setup Restify Server
-var server = restify.createServer();
-server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
-server.listen(process.env.port || 3978, function () {
-  console.log('%s listening to %s', server.name, server.url);
 });
 
 
--- public/javascripts/sugerencias.js
+++ public/javascripts/sugerencias.js
@@ -1,6 +1,16 @@
 var myApp = angular.module('mlapp', []);
+var URL_LUIS = 'https://api.projectoxford.ai/luis/v1/application?id=07f167d0-e823-4a36-b546-1502fe71c0c9&subscription-key=d6e2344416744f90b1a325501fd40a21&q=';
 
-myApp.controller('mlcontroller', ['$scope', function($scope) {
-    $scope.cities=["sdfsdfd", "sdf"];
+myApp.controller('mlcontroller',  function($scope,$http) {
+
+    $scope.askMelisa = function(query){
+      var fullPath = URL_LUIS+query;
+      console.log(fullPath);
+      $http.get(fullPath)
+        .then(function(response) {
+          console.log(response.data.intents[0].intent);
+            $scope.resp = response.data.intents[0].intent;
+        });
     }
-]);
+  }
+);
