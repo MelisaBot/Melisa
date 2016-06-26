b4bb1aafbe07ed640c8c83de2f4ec3875f99c781
Max <maxicespedes7@hotmail.com>
2016-06-26 03:34:35 -0300

    Added price searching in melis API

--- public/javascripts/sugerencias.js
+++ public/javascripts/sugerencias.js
@@ -1,18 +1,23 @@
 var myApp = angular.module('mlapp', []);
 var URL_LUIS = 'https://api.projectoxford.ai/luis/v1/application?id=07f167d0-e823-4a36-b546-1502fe71c0c9&subscription-key=d6e2344416744f90b1a325501fd40a21&q=';
 
-myApp.controller('mlcontroller',  function($scope,$http) {
+myApp.controller('mlcontroller', function($scope,$http) {
 
     $scope.askMelisa = function(query){
       if (query.length <4 ) {
+        $scope.resp ='';
         return;
       }
       var fullPath = URL_LUIS+query;
 
       $http.get(fullPath)
         .then(function(response) {
-            console.log(response.data.intents[0].intent);
-            $scope.resp = response.data.intents[0].intent;
+            $http.get('http://localhost:3000/publication')
+              .then(function(response){
+                console.log(response);
+                  $scope.price = response.data.price;
+              })
+              $scope.resp = response.data.intents[0].intent;
         });
     }
   }
